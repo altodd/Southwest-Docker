6980f03b5c6454fc9a12defc6b423165a89d42ea
hildebrau <hildebrau@users.noreply.github.com>
2023-12-06 21:09:42 -0600

    Update entrypoint.sh
    required elevated permissions now

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" != "/bin/sh" ]; then
 	sudo service atd start
 	sudo service cron start
 	cd /headers/southwest-headers/
-	env/bin/python southwest-headers.py /headers/southwest_headers.json
+	sudo env/bin/python southwest-headers.py /headers/southwest_headers.json
 	autoluv "$@"
 	while [ $(atq | wc -l) -gt 0 ]
 	do
